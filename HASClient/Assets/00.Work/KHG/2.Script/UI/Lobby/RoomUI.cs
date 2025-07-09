35a74279f42751293bed475cc5633e2e446b3b82
고한결 <gyeol0411@naver.com>
2025-07-09 19:41:38 +0900

    .update RoomConnect

--- HASClient/Assets/00.Work/KHG/2.Script/UI/Lobby/RoomUI.cs
+++ HASClient/Assets/00.Work/KHG/2.Script/UI/Lobby/RoomUI.cs
@@ -1,18 +1,40 @@
+using AKH.Network;
+using DewmoLib.Utiles;
+using KHG.Events;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class RoomUI : MonoBehaviour
 {
     [SerializeField] private Transform roomHandleTrm;
+    [SerializeField] private EventChannelSO packetChannel;
 
     [SerializeField] private GameObject roomPrefab;
 
+    private void Awake()
+    {
+        packetChannel.AddListener<RoomListEvent>(HandleRoomList);
+    }
+     
+    private void Start()
+    {
+        RequestRoomList();
+    }
+
     public void CreateRoomList(List<RoomInfoPacket> list)
     {
+        RemoveChildren(roomHandleTrm);
+
         for (int i = 0; i < list.Count; i++)
         {
             BuildRoom(list[i]);
         }
+    }
+
+    public void RequestRoomList()
+    {
+        C_RoomList c_RoomList = new C_RoomList();
+        NetworkManager.Instance.SendPacket(c_RoomList);
     }
 
     private void BuildRoom(RoomInfoPacket roomInfo)
@@ -22,6 +44,19 @@ public class RoomUI : MonoBehaviour
         if (roomBar.TryGetComponent(out RoomBar roomUi))
         {
             roomUi.SetRoomInfo(roomInfo);
+        }
+    }
+
+    private void HandleRoomList(RoomListEvent evt)
+    {
+        CreateRoomList(evt.infoPackets);
+    }
+
+    private void RemoveChildren(Transform target)
+    {
+        foreach (Transform child in target)
+        {
+            Destroy(child);
         }
     }
 }
