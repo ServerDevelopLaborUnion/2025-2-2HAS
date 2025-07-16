998aa454d7fbf90298d59834437f439b0aa59150
고한결 <gyeol0411@naver.com>
2025-07-16 19:45:38 +0900

    .update connectPanel

--- HASClient/Assets/00.Work/KHG/2.Script/Manager/RoomManager.cs
+++ HASClient/Assets/00.Work/KHG/2.Script/Manager/RoomManager.cs
@@ -1,12 +1,44 @@
+using Core.EventSystem;
+using DewmoLib.Dependencies;
+using DewmoLib.Utiles;
+using KHG.Events;
+using KHG.Managers;
 using UnityEngine;
 
 namespace KHG.UIs
 {
     public class RoomManager : MonoBehaviour
     {
-        private void Start()
-        {
+        [Inject] private PacketResponsePublisher _publisher;
+        [SerializeField] private EventChannelSO packetChannel;
+        [SerializeField] private EventChannelSO uiChannel;
 
+        [SerializeField] private PanelController connectPanelController;
+
+        private void Awake()
+        {
+            _publisher.AddListener(PacketID.C_RoomEnter, IsEnterSuccess);
+            _publisher.AddListener(PacketID.C_CreateRoom, IsEnterSuccess);
+            uiChannel.AddListener<ServerConnectEvent>(HandleConnectReq);
+        }
+
+        private void OnDestroy()
+        {
+            _publisher.RemoveListener(PacketID.C_RoomEnter, IsEnterSuccess);
+            _publisher.RemoveListener(PacketID.C_CreateRoom, IsEnterSuccess);
+            uiChannel.RemoveListener<ServerConnectEvent>(HandleConnectReq);
+        }
+
+        private void IsEnterSuccess(bool value)
+        {
+            connectPanelController.Close();
+
+            SceneLoadController.LoadScene("SampleScene");
+        }
+
+        private void HandleConnectReq(ServerConnectEvent evt)
+        {
+            connectPanelController.Open();
         }
     }
 }
