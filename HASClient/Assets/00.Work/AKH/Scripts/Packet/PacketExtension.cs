6fb3aba948287696692e181fb9a3169b1180c184
machany <machanicaly@gmail.com>
2025-07-16 19:35:46 +0900

    .

---
+++ HASClient/Assets/00.Work/AKH/Scripts/Packet/PacketExtension.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets._00.Work.AKH.Scripts.Packet
+{
+    public static class PacketExtension
+    {
+        public static VectorPacket ToPacket(this Vector3 vector)
+            => new VectorPacket() { x = vector.x, y = vector.y, z = vector.z };
+        public static Vector3 ToVector3(this VectorPacket packet)
+            => new Vector3() { x = packet.x, y = packet.y, z = packet.z };
+        public static Quaternion ToQuaternion(this QuaternionPacket packet)
+            => new Quaternion(packet.x, packet.y, packet.z, packet.w);
+        public static QuaternionPacket ToPacket(this Quaternion quaterion)
+            => new QuaternionPacket() { z = quaterion.z, w = quaterion.w, x = quaterion.x, y = quaterion.y };
+    }
+}
--- HASClient/Assets/00.Work/CDH/Code/PacketHandler.cs
+++ HASClient/Assets/00.Work/CDH/Code/PacketHandler.cs
@@ -1,8 +1,11 @@
 using _00.Work.CDH.Code;
+using _00.Work.CDH.Code.ChatFolder;
 using Assets._00.Work.CDH.Code.ChatFolder;
+using Assets._00.Work.CDH.Code.DummyClients;
 using ServerCore;
 using System.Diagnostics;
 using UnityEngine.iOS;
+using UnityEngine.Rendering;
 
 public partial class PacketHandler
 {
@@ -10,10 +13,23 @@ public partial class PacketHandler
     {
         S_Chat chat = (S_Chat)packet;
 
-        ChatRecvEventHandler chatEventHandler = ChatPacketEvents.ChatEvent;
+        ChatRecvEventHandler chatEventHandler = PacketEvents.ChatEvent;
         chatEventHandler.pName = chat.pName;
         chatEventHandler.message = chat.text;
 
         _packetChannel.InvokeEvent(chatEventHandler);
+    }
+
+    public void S_DummyClientHandler(PacketSession session, IPacket packet)
+    {
+        S_Move move = (S_Move)packet;
+
+        DummyClientMoveEventHandler dummyClientMoveEventHandler = PacketEvents.dummyClientMoveEvent;
+        dummyClientMoveEventHandler.index = move.index;
+        dummyClientMoveEventHandler.velocity = move.velocity;
+        dummyClientMoveEventHandler.position = move.position;
+        dummyClientMoveEventHandler.direction = move.direction;
+
+        _packetChannel.InvokeEvent(dummyClientMoveEventHandler);
     }
 }
