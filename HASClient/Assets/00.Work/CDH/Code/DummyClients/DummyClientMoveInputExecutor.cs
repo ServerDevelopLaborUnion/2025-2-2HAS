b7cf6c7217fa7980d8e8710125295cd39c452077
Choidongho08 <157602117+Choidongho08@users.noreply.github.com>
2025-07-16 19:27:09 +0900

    feat : DummyClient Move Executor

--- HASClient/Assets/00.Work/CDH/Code/DummyClients/DummyClientMoveInputExecutor.cs
+++ HASClient/Assets/00.Work/CDH/Code/DummyClients/DummyClientMoveInputExecutor.cs
@@ -1,19 +1,43 @@
-﻿using DewmoLib.Utiles;
+﻿using Assets._00.Work.YHB.Scripts.Entities;
+using Assets._00.Work.YHB.Scripts.ExecuteBehaviour;
+using Assets._00.Work.YHB.Scripts.ExecuteBehaviour.DataTypes;
+using DewmoLib.Utiles;
+using System;
 using UnityEngine;
 
 namespace Assets._00.Work.CDH.Code.DummyClients
 {
     public class DummyClientMoveInputExecutor : MonoBehaviour
     {
+        [Header("Value")]
         [SerializeField] private EventChannelSO packetChannel;
+        [SerializeField] private ScriptableBehaviourSO moveInputBehaviour;
+        [SerializeField] private ScriptableBehaviourSO jumpInputBehaviour;
+
+        [Header("Value")]
+        [SerializeField] private EntityMovement entityMovement;
+
+        private EntityMovementData _moveData;
 
         private void Awake()
         {
-            
+            _moveData = new EntityMovementData();
+            _moveData.entityMovement = entityMovement;
         }
 
         private void Start()
         {
+            packetChannel.AddListener<DummyClientMoveEventHandler>(MoveEventHandler);
+        }
+
+        private void MoveEventHandler(DummyClientMoveEventHandler evt)
+        {
+            Vector2 moveDirection = new Vector2(evt.direction.x, evt.direction.z);
+            _moveData.moveDirection = moveDirection;
+            // _moveData.moveRotation = evt.
+
+            // jumpInputBehaviour.Execute<EntityMovementData>(_moveData);
+            moveInputBehaviour.Execute<EntityMovementData>(_moveData);
         }
     }
 }
