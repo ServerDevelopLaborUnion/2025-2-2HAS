d249d71bff96c73bc89a6555b4862be4d5859bbb
고한결 <gyeol0411@naver.com>
2025-07-19 18:13:45 +0900

    .

--- HASClient/Assets/00.Work/KHG/2.Script/Manager/EntityManager.cs
+++ HASClient/Assets/00.Work/KHG/2.Script/Manager/EntityManager.cs
@@ -6,58 +6,58 @@ namespace KHG.Managers
 {
     public class EntityManager : MonoBehaviour
     {
-        private static EntityManager _instance;
-        public static EntityManager Instance => _instance;
-        [SerializeField] private SerializedDictionary<ObjectType, GameObject> _prefabs;
-        private Dictionary<int, GameObject> _objects = new();
-        private void Awake()
-        {
-            if (_instance == null)
-                _instance = this;
-        }
-        public GameObject CreateObject<T>(int index, ObjectType type, Vector3 position, Quaternion quat, out T compo) where T : MonoBehaviour
-        {
-            GameObject obj = Instantiate(_prefabs[type], position, quat);
-            _objects.Add(index, obj);
-            compo = obj.GetComponent<T>();
-            return compo.gameObject;
-        }
-        public T CreateObject<T>(int index, GameObject prefab, Vector3 position, Quaternion quat)
-        {
-            GameObject obj = Instantiate(prefab, position, quat);
-            _objects.Add(index, obj);
-            return obj.GetComponent<T>();
-        }
-        public T CreateObject<T>(int index, ObjectType type)
-        {
-            GameObject obj = Instantiate(_prefabs[type]);
-            _objects.Add(index, obj);
-            return obj.GetComponent<T>();
-        }
-        public T GetObject<T>(int index)
-        {
-            GameObject obj = _objects.GetValueOrDefault(index);
-            return obj.GetComponent<T>();
-        }
-        public T GetObjectOrCreate<T>(int index, ObjectType type)
-        {
-            GameObject obj = _objects.GetValueOrDefault(index);
-            if (obj == null)
-                return CreateObject<T>(index, type);
-            else
-                return obj.GetComponent<T>();
-        }
-        public void RemoveObject(int index)
-        {
-            Destroy(_objects[index]);
-            _objects.Remove(index);
-        }
-        public void DestroyAll()
-        {
-            foreach (var item in _objects)
-                Destroy(item.Value.gameObject);
-            _objects.Clear();
-        }
+        //private static EntityManager _instance;
+        //public static EntityManager Instance => _instance;
+        //[SerializeField] private SerializedDictionary<ObjectType, GameObject> _prefabs;
+        //private Dictionary<int, GameObject> _objects = new();
+        //private void Awake()
+        //{
+        //    if (_instance == null)
+        //        _instance = this;
+        //}
+        //public GameObject CreateObject<T>(int index, ObjectType type, Vector3 position, Quaternion quat, out T compo) where T : MonoBehaviour
+        //{
+        //    GameObject obj = Instantiate(_prefabs[type], position, quat);
+        //    _objects.Add(index, obj);
+        //    compo = obj.GetComponent<T>();
+        //    return compo.gameObject;
+        //}
+        //public T CreateObject<T>(int index, GameObject prefab, Vector3 position, Quaternion quat)
+        //{
+        //    GameObject obj = Instantiate(prefab, position, quat);
+        //    _objects.Add(index, obj);
+        //    return obj.GetComponent<T>();
+        //}
+        //public T CreateObject<T>(int index, ObjectType type)
+        //{
+        //    GameObject obj = Instantiate(_prefabs[type]);
+        //    _objects.Add(index, obj);
+        //    return obj.GetComponent<T>();
+        //}
+        //public T GetObject<T>(int index)
+        //{
+        //    GameObject obj = _objects.GetValueOrDefault(index);
+        //    return obj.GetComponent<T>();
+        //}
+        //public T GetObjectOrCreate<T>(int index, ObjectType type)
+        //{
+        //    GameObject obj = _objects.GetValueOrDefault(index);
+        //    if (obj == null)
+        //        return CreateObject<T>(index, type);
+        //    else
+        //        return obj.GetComponent<T>();
+        //}
+        //public void RemoveObject(int index)
+        //{
+        //    Destroy(_objects[index]);
+        //    _objects.Remove(index);
+        //}
+        //public void DestroyAll()
+        //{
+        //    foreach (var item in _objects)
+        //        Destroy(item.Value.gameObject);
+        //    _objects.Clear();
+        //}
     }
 
     public enum ObjectType
